99d462cb463e921d16f867c28844d6052f3cd140
gnue <gnue@so-kukan.com>
2014-03-19 17:40:15 +0900

    コマンド引数の -d フラグで簡易出力とダンプ出力を指定できるようにした

--- felica-look.go
+++ felica-look.go
@@ -2,26 +2,63 @@ package main
 
 import (
 	"./felica"
+	"flag"
 	"fmt"
 	"os"
+	"path"
 )
 
-func main() {
-	for _, v := range os.Args[1:] {
-		cardinfo := felica.Read(v)
+// コマンドの使い方
+func usage() {
+	cmd := os.Args[0]
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", path.Base(cmd))
+	flag.PrintDefaults()
+	os.Exit(0)
+}
 
-		for syscode, currsys := range *cardinfo {
-			fmt.Println("SYSTEM CODE: ", syscode)
-			fmt.Println("  IDm: ", currsys.IDm())
-			fmt.Println("  PMm: ", currsys.PMm())
+// カード情報を簡易出力する
+func show_info(cardinfo *felica.CardInfo) {
+	for syscode, currsys := range *cardinfo {
+		fmt.Println("SYSTEM CODE: ", syscode)
+		fmt.Println("  IDm: ", currsys.IDm())
+		fmt.Println("  PMm: ", currsys.PMm())
+	}
+}
 
-			for svccode, data := range currsys.Services() {
-				fmt.Println("  SERVICE CODE: ", svccode)
+// カード情報をダンプ出力する
+func dump_info(cardinfo *felica.CardInfo) {
+	for syscode, currsys := range *cardinfo {
+		fmt.Println("SYSTEM CODE: ", syscode)
+		fmt.Println("  IDm: ", currsys.IDm())
+		fmt.Println("  PMm: ", currsys.PMm())
 
-				for _, v := range data {
-					fmt.Printf("      %X\n", v)
-				}
+		for svccode, data := range currsys.Services() {
+			fmt.Println("  SERVICE CODE: ", svccode)
+
+			for _, v := range data {
+				fmt.Printf("      %X\n", v)
 			}
 		}
+	}
+}
+
+func main() {
+	dump := flag.Bool("d", false, "dump")
+	help := flag.Bool("h", false, "help")
+	flag.Parse()
+
+	if *help || len(flag.Args()) == 0 {
+		usage()
+	}
+
+	for _, v := range flag.Args() {
+		cardinfo := felica.Read(v)
+
+		if *dump {
+			dump_info(cardinfo)
+		} else {
+			show_info(cardinfo)
+		}
+
 	}
 }
