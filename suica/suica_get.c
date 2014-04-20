cabbab5e53c46d55bda220369c5cb5df9f741292
gnue <gnue@so-kukan.com>
2014-04-20 19:04:51 +0900

    suica 関数の引数を const にするようにした

--- suica/suica_get.c
+++ suica/suica_get.c
@@ -15,12 +15,12 @@ static int bytes_to_int(const uint8_t bytes[], size_t len) {
 
 // *** Suica利用履歴
 // 端末種
-int suica_value_type(suica_value_t *value) {
+int suica_value_type(const suica_value_t *value) {
 	return value->type;
 }
 
 // 利用年月日
-time_t suica_value_date(suica_value_t *value) {
+time_t suica_value_date(const suica_value_t *value) {
 	int day = suica_day(value);
 	if (day == 0) return 0;
 
@@ -34,21 +34,21 @@ time_t suica_value_date(suica_value_t *value) {
 }
 
 // 入場駅（線区コード、駅順コード）
-int suica_value_in_station(suica_value_t *value) {
+int suica_value_in_station(const suica_value_t *value) {
 	return bytes_to_int(value->in_station, sizeof(value->in_station));
 }
 
 // 出場駅（線区コード、駅順コード）
-int suica_value_out_station(suica_value_t *value) {
+int suica_value_out_station(const suica_value_t *value) {
 	return bytes_to_int(value->out_station, sizeof(value->out_station));
 }
 
 // 残額（リトルエンディアン）
-int suica_value_balance(suica_value_t *value) {
+int suica_value_balance(const suica_value_t *value) {
 	return suica_balance(value);
 }
 
 // 連番
-int suica_value_no(suica_value_t *value) {
+int suica_value_no(const suica_value_t *value) {
 	return bytes_to_int(value->no, sizeof(value->no));
 }
