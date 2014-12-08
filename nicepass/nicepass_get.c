dcaa6dfe933ebea6ad8f152a7bf0f3771ad503d7
gnue <gnue@so-kukan.com>
2014-12-08 16:52:10 +0900

    nice-pass属性データの取得関数

--- nicepass/nicepass_get.c
+++ nicepass/nicepass_get.c
@@ -12,9 +12,94 @@ static int bytes_to_int(const uint8_t bytes[], size_t len) {
 	return value;
 }
 
+// *** nice-pass残額
+// チャージ金額残額
+int nicepass_amount_charge(const nicepass_amount_t *amount) {
+	return bytes_to_int(amount->charge, sizeof(amount->charge));
+}
+
+// プレミアム残額種別
+int nicepass_amount_premium_kind(const nicepass_amount_t *amount) {
+	return nicepass_premium_kind(amount);
+}
+
+// プレミアム残額
+int nicepass_amount_premium(const nicepass_amount_t *amount) {
+	return nicepass_premium(amount);
+}
+
+// *** nice-pass属性情報(2)
+// 乗車日時
+time_t nicepass_attr_in_time(const nicepass_attr2_t *attr) {
+	int day = nicepass_day(attr);
+	if (day == 0) return 0;
+
+	struct tm tm = {
+		//.tm_sec = nicepass_sec(attr->in_time),
+		.tm_min = nicepass_min(attr->in_time),
+		.tm_hour = nicepass_hour(attr->in_time),
+		.tm_mday = day,
+		.tm_mon = nicepass_month(attr) - 1,
+		.tm_year = nicepass_year(attr) + 2000 - 1900,
+	};
+
+	return mktime(&tm);
+}
+
+// 降車日時
+time_t nicepass_attr_out_time(const nicepass_attr2_t *attr) {
+	int day = nicepass_day(attr);
+	if (day == 0) return 0;
+
+	struct tm tm = {
+		//.tm_sec = nicepass_sec(attr->out_time),
+		.tm_min = nicepass_min(attr->out_time),
+		.tm_hour = nicepass_hour(attr->out_time),
+		.tm_mday = day,
+		.tm_mon = nicepass_month(attr) - 1,
+		.tm_year = nicepass_year(attr) + 2000 - 1900,
+	};
+
+	return mktime(&tm);
+}
+
+// 使用装置
+int nicepass_attr_type(const nicepass_attr2_t *attr) {
+	return nicepass_type(attr);
+}
+
+// 処理種別
+int nicepass_attr_proc(const nicepass_attr2_t *attr) {
+	return nicepass_proc(attr);
+}
+
+// 直近利用金額
+int nicepass_attr_use(const nicepass_attr2_t *attr) {
+	return bytes_to_int(attr->use, sizeof(attr->use)) * 10;
+}
+
+// 直近残額
+int nicepass_attr_balance(const nicepass_attr2_t *attr) {
+	return nicepass_balance(attr);
+}
+
+// *** nice-pass属性情報(3)
+// 乗車駅
+int nicepass_attr_in_station(const nicepass_attr3_t *attr) {
+	return nicepass_in_station(attr);
+}
+
+// 降車駅
+int nicepass_attr_out_station(const nicepass_attr3_t *attr) {
+	return nicepass_out_station(attr);
+}
+
+// 取引通番
+int nicepass_attr_no(const nicepass_attr3_t *attr) {
+	return bytes_to_int(attr->no, sizeof(attr->no));
+}
 
 // *** nice-pass利用履歴
-
 // 処理日時
 time_t nicepass_value_datetime(const nicepass_value_t *value) {
 	int day = nicepass_day(value);
--- nicepass/nicepass_get.h
+++ nicepass/nicepass_get.h
@@ -1,11 +1,27 @@
 #include <time.h>
 #include "nicepass.h"
 
+// *** nice-pass残額
+int nicepass_amount_charge(const nicepass_amount_t *amount);		// チャージ金額残額
+int nicepass_amount_premium_kind(const nicepass_amount_t *amount);	// プレミアム残額種別
+int nicepass_amount_premium(const nicepass_amount_t *amount);		// プレミアム残額
+
+// *** nice-pass属性情報
+time_t nicepass_attr_in_time(const nicepass_attr2_t *attr);		// 乗車日時
+time_t nicepass_attr_out_time(const nicepass_attr2_t *attr);	// 降車日時
+int nicepass_attr_type(const nicepass_attr2_t *attr);			// 使用装置
+int nicepass_attr_proc(const nicepass_attr2_t *attr);			// 処理種別
+int nicepass_attr_use(const nicepass_attr2_t *attr);			// 直近利用金額
+int nicepass_attr_balance(const nicepass_attr2_t *attr);		// 直近残額
+int nicepass_attr_in_station(const nicepass_attr3_t *attr);		// 乗車駅
+int nicepass_attr_out_station(const nicepass_attr3_t *attr);	// 降車駅
+int nicepass_attr_no(const nicepass_attr3_t *attr);				// 取引通番
+
 // *** nice-pass利用履歴
 time_t nicepass_value_datetime(const nicepass_value_t *value);	// 処理日時
 int nicepass_value_train(const nicepass_value_t *value);		// 装置番号
-int nicepass_value_in_station(const nicepass_value_t *value);	// 入場駅
-int nicepass_value_out_station(const nicepass_value_t *value);	// 出場駅
+int nicepass_value_in_station(const nicepass_value_t *value);	// 乗車駅
+int nicepass_value_out_station(const nicepass_value_t *value);	// 降車駅
 int nicepass_value_type(const nicepass_value_t *value);			// 使用装置
 int nicepass_value_proc(const nicepass_value_t *value);			// 処理種別
 int nicepass_value_use_kind(const nicepass_value_t *value);		// 利用金額種別
